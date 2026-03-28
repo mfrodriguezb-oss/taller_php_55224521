1d3bda0bbaba09d4d2abd6258a63cccee655a882
Michelle Rodriguez <mfrodriguezb@uniboyaca.edu.co>
2026-03-28 12:46:36 -0500

    Aplicación de estilos con css a la página del menú principal

--- index.php
+++ index.php
@@ -3,42 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Taller PHP - Menú Principal</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 40px;
-        }
-        ul {
-            list-style-type: none;
-            padding: 0;
-        }
-        li {
-            margin: 10px 0;
-        }
-        a {
-            text-decoration: none;
-            font-weight: bold;
-            color: #2c3e50;
-        }
-        a:hover {
-            color: #2980b9;
-        }
-    </style>
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
 
-<h1>Taller de PHP</h1>
-<h3>Menú Principal</h3>
+<div class="container">
 
-<ul>
-    <li><a href="acronimo/index.php">Punto 1: Acrónimos</a></li>
-    <li><a href="fibonacci_factorial/index.php">Punto 2: Fibonacci y Factorial</a></li>
-    <li><a href="media_mediana_moda/index.php">Punto 3: Media, Mediana y Moda</a></li>
-    <li><a href="conjuntos/index.php">Punto 4: Conjuntos</a></li>
-    <li><a href="binario/index.php">Punto 5: Conversión a Binario</a></li>
-    <li><a href="arbol_binario/index.php">Punto 6: Árbol Binario</a></li>
-    <li><a href="calculadora/index.php">Punto 7: Calculadora con Historial</a></li>
-</ul>
+    
+    <h1>Menú Principal</h1>
+
+    <ul>
+        <li><a href="acronimo/index.php">Punto 1: Acrónimos</a></li>
+        <li><a href="fibonacci_factorial/index.php">Punto 2: Fibonacci y Factorial</a></li>
+        <li><a href="media_mediana_moda/index.php">Punto 3: Media, Mediana y Moda</a></li>
+        <li><a href="conjuntos/index.php">Punto 4: Conjuntos</a></li>
+        <li><a href="binario/index.php">Punto 5: Conversión a Binario</a></li>
+        <li><a href="arbol_binario/index.php">Punto 6: Árbol Binario</a></li>
+        <li><a href="calculadora/index.php">Punto 7: Calculadora</a></li>
+    </ul>
+
+</div>
 
 </body>
 </html>
